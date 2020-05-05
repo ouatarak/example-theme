8d6abe515769830623d8a4cb1886084efdd8a5ef
ouatarak@yahoo.fr <hellokounandi.codes>
2020-05-04 18:48:05 -0600

    added example-themein wordpress

---
+++ singular.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+    <?php if ( have_posts() ) : ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+            <article>
+                <h2><?php the_title(); ?></h2>
+                <?php the_content(); // Controls HTML content. ?>
+
+            </article>
+            <nav>
+                <a href="<?php echo site_url(); ?>">   Go Back
+                </a>
+            </nav>
+        <?php endwhile; ?>
+    <?php endif; ?>
+
+<?php get_footer(); ?>
